e19977461bb8e66884c91d971fde1d52913a2cc0
Naman Shukla <namanshukla256@gmail.com>
2025-04-28 18:46:43 +0530

    cart session

--- Chapter 8 - Building an Online Shop/myshop/cart/cart.py
+++ Chapter 8 - Building an Online Shop/myshop/cart/cart.py
@@ -1,11 +1,13 @@
 from decimal import Decimal
+
 from django.conf import settings
 from shop.models import Product
+
 
 class Cart:
     def __init__(self, request):
         """
-        Initialize the cart
+        Initialize the cart.
         """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
@@ -30,16 +32,22 @@ class Cart:
             item['total_price'] = item['price'] * item['quantity']
             yield item
 
+    def __len__(self):
+        """
+        Count all items in the cart.
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+
     def add(self, product, quantity=1, override_quantity=False):
-        """Add a product to the cart or update its quanity
+        """
+        Add a product to the cart or update its quantity.
         """
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {
                 'quantity': 0,
-                'price': str(product.price)
+                'price': str(product.price),
             }
-
         if override_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
@@ -58,3 +66,14 @@ class Cart:
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
+    def clear(self):
+        # remove cart from session
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+
+    def get_total_price(self):
+        return sum(
+            Decimal(item['price']) * item['quantity']
+            for item in self.cart.values()
+        )
